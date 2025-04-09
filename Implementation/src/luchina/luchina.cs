c22b9464296964ef8a5c2007278bb4d0ef5cb184
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2025-04-09 19:37:53 +0300

    Add files via upload

--- Implementation/src/luchina/luchina.cs
+++ Implementation/src/luchina/luchina.cs
@@ -1,1 +1,22 @@
+﻿﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2019-2025. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
 
+using System;
+using System.Reflection;
+
+public static unsafe partial class luchina
+{
+
+    public static Version version
+    {
+
+        get
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version;
+        }
+
+    }
+
+}
