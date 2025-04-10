4f168b8caceda5681a250e43b1d28f926251e34f
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2025-04-10 21:51:39 +0300

    Add files via upload

---
+++ Implementation/src/luchina/luchina.DType.cs
@@ -0,0 +1,39 @@
+﻿﻿//***************************************************************************************************
+//* (C) ColorfulSoft corp., 2019-2025. All rights reserved.
+//* The code is available under the Apache-2.0 license. Read the License for details.
+//***************************************************************************************************
+
+public static unsafe partial class luchina
+{
+
+    public enum DType : byte
+    {
+
+        /// <summary>
+        /// 32-bit floating point. Alias for float.
+        /// </summary>
+        float32 = 0,
+
+        /// <summary>
+        /// 32-bit floating point. Alias for float32.
+        /// </summary>
+        @float = 0,
+
+        /// <summary>
+        /// 32-bit signed integer. Alias for int.
+        /// </summary>
+        int32 = 1,
+
+        /// <summary>
+        /// 32-bit signed integer. Alias for int32.
+        /// </summary>
+        @int = 1,
+
+        /// <summary>
+        /// Boolean.
+        /// </summary>
+        @bool = 2
+
+    }
+
+}
--- Implementation/src/luchina/luchina.cs
+++ Implementation/src/luchina/luchina.cs
@@ -24,6 +24,14 @@ public static unsafe partial class luchina
 
     }
 
+    /// <summary>
+    /// Performs initialization of the luchina module.
+    /// </summary>
+    static luchina()
+    {
+        default_dtype = float32;
+    }
+
     #endregion
 
     #region Device
@@ -49,6 +57,102 @@ public static unsafe partial class luchina
     public static Device device(string device)
     {
         return new Device(device);
+    }
+
+    #endregion
+
+    #region DType
+
+    /// <summary>
+    /// 32-bit floating point. Alias for luchina.float.
+    /// </summary>
+    public const DType float32 = DType.float32;
+
+    /// <summary>
+    /// 32-bit floating point. Alias for luchina.float32.
+    /// </summary>
+    public const DType @float = DType.float32;
+
+    /// <summary>
+    /// 32-bit signed integer. Alias for luchina.int.
+    /// </summary>
+    public const DType int32 = DType.int32;
+
+    /// <summary>
+    /// 32-bit signed integer. Alias for luchina.int32.
+    /// </summary>
+    public const DType @int = DType.int32;
+
+    /// <summary>
+    /// Boolean.
+    /// </summary>
+    public const DType @bool = DType.@bool;
+
+    /// <summary>
+    /// Gets or sets the default data type for tensors.
+    /// </summary>
+    public static DType default_dtype
+    {
+
+        get;
+
+        set;
+
+    }
+
+    /// <summary>
+    /// Returns the size of the data type value in bytes.
+    /// </summary>
+    /// <param name="dtype">Data type.</param>
+    /// <returns>Size in bytes.</returns>
+    public static int element_size(this DType dtype)
+    {
+        switch(dtype)
+        {
+            case @bool:
+            {
+                return 1;
+            }
+            case float32:
+            case int32:
+            {
+                return 4;
+            }
+            default:
+            {
+                throw new TypeAccessException(string.Format("Unknown data type with code {0}.", (byte)dtype));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Checks whether the specified data type is a boolean.
+    /// </summary>
+    /// <param name="dtype">Data type.</param>
+    /// <returns>true, if dtype is boolean.</returns>
+    public static bool is_boolean(this DType dtype)
+    {
+        return dtype == @bool;
+    }
+
+    /// <summary>
+    /// Checks whether the specified data type is a floating point.
+    /// </summary>
+    /// <param name="dtype">Data type.</param>
+    /// <returns>true, if dtype is floating point.</returns>
+    public static bool is_floating_point(this DType dtype)
+    {
+        return dtype == float32;
+    }
+
+    /// <summary>
+    /// Checks whether the specified data type is an integer.
+    /// </summary>
+    /// <param name="dtype">Data type.</param>
+    /// <returns>true, if dtype is integer.</returns>
+    public static bool is_integer(this DType dtype)
+    {
+        return dtype == int32;
     }
 
     #endregion
